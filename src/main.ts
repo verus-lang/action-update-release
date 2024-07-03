821d0c43d4d2852532663382a70f9dcbe6ba0b4b
Andrea Lattuada <andrea@lattuada.me>
2024-07-03 10:56:07 +0200

    remove post-action to allow more fine-grained control

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,13 @@ async function run(): Promise<void> {
     const commitish = github.context.sha;
     const delete_assets =
       Boolean(core.getInput('delete_assets', {required: false})) || false;
+    const delete_tags_prefix: string | null = core.getInput(
+      'delete_tags_prefix',
+      {required: false}
+    );
+    const new_draft_status: boolean | null = Boolean(
+      core.getInput('new_draft_status', {required: false})
+    );
 
     if (delete_assets) {
       // Delete all assets from release
@@ -52,23 +59,60 @@ async function run(): Promise<void> {
       }
     }
 
-    // Create a release
-    // API Documentation: https://developer.github.com/v3/repos/releases/#create-a-release
-    // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-create-release
-    const updateReleaseResponse = await octokit.rest.repos.updateRelease({
-      owner,
-      repo,
-      release_id: id,
-      tag_name: new_tag,
-      name: new_name,
-      body: new_body,
-      target_commitish: commitish
-    });
+    if (delete_tags_prefix !== null) {
+      // see https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repository-tags
+      const {data: tags} = await octokit.rest.repos.listTags({
+        owner,
+        repo
+      });
+
+      const tagsToBeDeleted = tags
+        .filter((tag: Tag) => tag.name.startsWith(delete_tags_prefix))
+        .filter((tag: Tag) => tag.name !== new_tag);
+
+      for (const tag of tagsToBeDeleted) {
+        await octokit.rest.git.deleteRef({
+          owner,
+          repo,
+          ref: `tags/${tag.name}`
+        });
+        core.info(`Tag '${tag.name}' was successfully deleted`);
+      }
+      core.info(`${tagsToBeDeleted.length} release(s) have been deleted`);
+    }
+
+    let getUpdateReleaseResponse;
+    if (
+      new_tag !== null ||
+      new_name !== null ||
+      new_body !== null ||
+      commitish !== null ||
+      new_draft_status !== null
+    ) {
+      // API Documentation: https://developer.github.com/v3/repos/releases
+      // Octokit Documentation: https://octokit.github.io/rest.js
+      getUpdateReleaseResponse = await octokit.rest.repos.updateRelease({
+        owner,
+        repo,
+        release_id: id,
+        tag_name: new_tag,
+        name: new_name,
+        body: new_body,
+        target_commitish: commitish,
+        draft: new_draft_status
+      });
+    } else {
+      getUpdateReleaseResponse = await octokit.rest.repos.getRelease({
+        owner,
+        repo,
+        release_id: id
+      });
+    }
 
     // Get the ID, html_url, and upload URL for the created Release from the response
     const {
       data: {id: releaseId, html_url: htmlUrl, upload_url: uploadUrl}
-    } = updateReleaseResponse;
+    } = getUpdateReleaseResponse;
 
     // Set the output variables for use by other actions: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
     core.setOutput('id', releaseId);
@@ -81,6 +125,19 @@ async function run(): Promise<void> {
       core.setFailed(`unknown error type ${error}`);
     }
   }
+}
+
+interface Tag {
+  name: string;
+  commit: Commit;
+  zipball_url: string;
+  tarball_url: string;
+  node_id: string;
+}
+
+interface Commit {
+  sha: string;
+  url: string;
 }
 
 run();
